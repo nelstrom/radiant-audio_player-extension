55db39b34fb3943029214c9e6d764e31ab0580b4
Andrew Neil <andrew.jr.neil@gmail.com>
2008-10-18 13:07:51 +0100

    Added routes as required by the AbstractModelController. Activated the "Audio" tab, and set up an "uncountable" inflection for audio (i.e. not "audios"!).

--- audio_player_extension.rb
+++ audio_player_extension.rb
@@ -3,12 +3,24 @@ class AudioPlayerExtension < Radiant::Extension
   description "Easily upload mp3 files, and embed them on your site with a Flash audio player."
   url "http://github.com/nelstrom/radiant-audio_player-extension/tree/master"
   
-  # define_routes do |map|
-  #   map.connect 'admin/audio_player/:action', :controller => 'admin/audio_player'
-  # end
+  define_routes do |map|
+    # map.connect 'admin/audio_player/:action', :controller => 'admin/audio_player'
+    map.with_options(:controller => 'admin/audio') do |link|
+      link.link_index           'admin/audio',             :action => 'index'
+      link.link_new             'admin/audio/new',         :action => 'new'
+      link.link_edit            'admin/audio/edit/:id',    :action => 'edit'
+      link.link_remove          'admin/audio/remove/:id',  :action => 'remove'
+    end
+    
+    
+  end
   
   def activate
-    # admin.tabs.add "Audio Player", "/admin/audio_player", :after => "Layouts", :visibility => [:all]
+    Inflector.inflections do |inflect|
+      inflect.uncountable "audio" # cause 'audios' just sounds wrong!
+    end
+    
+    admin.tabs.add "Audio", "/admin/audio", :after => "Layouts", :visibility => [:all]
   end
   
   def deactivate
