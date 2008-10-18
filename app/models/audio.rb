0d992082cd8201042720c6023ea9678161374822
Andrew Neil <andrew.jr.neil@gmail.com>
2008-10-18 17:26:41 +0100

    Refined the audio model, with relationships to user, uniqueness on title, and specs on attachment path and url. Added an upload file field to the Audio edit form.

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,4 +1,10 @@
 class Audio < ActiveRecord::Base
-  has_attached_file :track
+  has_attached_file :track,
+    :url => "/:class/:id/:basename.:extension",
+    :path => ":rails_root/public/:class/:id/:basename.:extension"
   
+  belongs_to :created_by, :class_name => 'User'
+  belongs_to :updated_by, :class_name => 'User'
+  
+  validates_uniqueness_of :title, :message => 'name already in use'
 end
