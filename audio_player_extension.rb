41fb089c14d1f62bd3e8341ed8fd9571990b66b1
Andrew Neil <andrew.jr.neil@gmail.com>
2008-10-18 11:49:52 +0100

    Updated the version, description and url.

--- audio_player_extension.rb
+++ audio_player_extension.rb
@@ -1,10 +1,7 @@
-# Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application'
-
 class AudioPlayerExtension < Radiant::Extension
-  version "1.0"
-  description "Describe your extension here"
-  url "http://yourwebsite.com/audio_player"
+  version "0.1"
+  description "Easily upload mp3 files, and embed them on your site with a Flash audio player."
+  url "http://github.com/nelstrom/radiant-audio_player-extension/tree/master"
   
   # define_routes do |map|
   #   map.connect 'admin/audio_player/:action', :controller => 'admin/audio_player'
