5e2b16459fcb174908d07bf91a914adb66a07d62
Andrew Neil <andrew.jr.neil@gmail.com>
2008-10-21 08:40:55 +0100

    Refactored the audio_tags, to share code in context of track or tracks:each.

--- lib/audio_tags.rb
+++ lib/audio_tags.rb
@@ -29,39 +29,42 @@ module AudioTags
   
   # ------------
   
-  
+  ["track", "tracks:each"].each do |context|
+    
+    [:title, :description].each do |column|
+      desc %{  Renders contents unless the `#{column}' attribute is blank.}
+      tag "#{context}:if_#{column}" do |tag|
+        tag.expand unless tag.locals.audio_track[column].blank?
+      end
+    end
+    
     desc %{Renders the title of the current audio track}
-    tag "track:title" do |tag|
+    tag "#{context}:title" do |tag|
       tag.locals.audio_track.title
     end
 
-    desc %{Renders the tag contents only if the description is not blank}
-    tag "track:if_description" do |tag|
-      tag.expand unless tag.locals.audio_track.description.blank?
-    end
-
     desc %{Renders the tag contents only if the description is blank}
-    tag "track:unless_description" do |tag|
+    tag "#{context}:unless_description" do |tag|
       tag.expand if tag.locals.audio_track.description.blank?
     end
 
     desc %{Renders the description of the current audio track}
-    tag "track:description" do |tag|
+    tag "#{context}:description" do |tag|
       tag.locals.audio_track.description_with_filter
     end
 
     desc %{Shows the path for this audio track}
-    tag "track:path" do |tag|
+    tag "#{context}:path" do |tag|
       tag.locals.audio_track.path
     end
 
     desc %{Shows the url for this audio track (ie, where it can be downloaded from)}
-    tag 'track:url' do |tag|
+    tag "#{context}:url" do |tag|
       tag.locals.audio_track.url
     end
 
     desc %{Embed a flash player for this audio track}
-    tag 'track:player' do |tag|
+    tag "#{context}:player" do |tag|
       %Q{<object type="application/x-shockwave-flash" data="/flash/audio_player/player.swf" id="audioplayer#{tag.locals.audio_track.id}" height="24" width="290">
   <param name="movie" value="/flash/audio_player/player.swf">
   <param name="FlashVars" value="#{player_params(tag.locals.audio_track)}">
@@ -70,6 +73,24 @@ module AudioTags
   <param name="wmode" value="transparent">
   </object>}
     end
+    
+    desc %{
+      Provides a link to the virtual page for this audio track.
+
+      Note that this tag will only work if your site has one page of type AudioPage. 
+      If you have more than one Audio Page, or no Audio Pages, then this tag won't work.
+    }
+    tag "#{context}:link" do |tag|
+      track = tag.locals.audio_track
+      if path = track.path
+        text = tag.double? ? tag.expand : track.title
+        %{<a href="#{path}" title="#{track.title}">#{text}</a>}
+      else
+        "You must have exactly 1 Audio page for this tag to work."
+      end
+    end
+    
+  end
   
   # ------------
   
@@ -87,51 +108,6 @@ module AudioTags
       audio_tracks << tag.expand
     end
     audio_tracks
-  end
-  
-  desc %{  Renders the title of the current audio track.}
-  tag "tracks:each:title" do |tag|
-    tag.locals.audio_track.title
-  end
-  
-  desc %{  Renders the title of the current audio track.}
-  tag "tracks:each:description" do |tag|
-    tag.locals.audio_track.description_with_filter
-  end
-  
-  [:title, :description].each do |column|
-    desc %{  Renders contents unless the `#{column}' attribute is blank.}
-    tag "movies:each:if_#{column}" do |tag|
-      tag.expand unless tag.locals.audio_track[column].blank?
-    end
-  end
-  
-  desc %{
-    Embeds the current audio track on the page, using the flash player.}
-  tag 'tracks:each:player' do |tag|
-    %Q{<object type="application/x-shockwave-flash" data="/flash/audio_player/player.swf" id="audioplayer#{tag.locals.audio_track.id}" height="24" width="290">
-<param name="movie" value="/flash/audio_player/player.swf">
-<param name="FlashVars" value="#{player_params(tag.locals.audio_track)}">
-<param name="quality" value="high">
-<param name="menu" value="false">
-<param name="wmode" value="transparent">
-</object>}
-  end
-  
-  desc %{
-    Provides a link to the virtual page for this audio track.
-    
-    Note that this tag will only work if your site has one page of type AudioPage. 
-    If you have more than one Audio Page, or no Audio Pages, then this tag won't work.
-  }
-  tag 'tracks:each:link' do |tag|
-    track = tag.locals.audio_track
-    if path = track.path
-      text = tag.double? ? tag.expand : track.title
-      %{<a href="#{path}" title="#{track.title}">#{text}</a>}
-    else
-      "You must have exactly 1 Audio page for this tag to work."
-    end
   end
   
   private
