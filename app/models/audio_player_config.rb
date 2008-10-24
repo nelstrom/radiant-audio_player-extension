72af42ee94d006665a0e9c2ff2616c759ab627b5
Andrew Neil <andrew.jr.neil@gmail.com>
2008-10-24 08:40:23 +0100

    Added methods to the AudioPlayerConfig model for fetching parameters in the format expected by the Flash.

---
+++ app/models/audio_player_config.rb
@@ -0,0 +1,31 @@
+class AudioPlayerConfig < ActiveRecord::Base
+  validates_presence_of :name
+  validates_uniqueness_of :name, :message => 'name already in use'
+  
+  def parameters_for_player
+    pa = []
+    parameters.each do |key,value|
+      pa << "#{key}=#{value}"
+    end
+    pa.join("&amp;")
+  end
+  
+  def parameters
+    cols = {}
+    colors.each do |key|
+      value = self.send(key)
+      unless value.blank?
+        cols[key] = value
+      end
+    end
+    bools = {}
+    bools[:loop] = (self.loop ? "yes" : "no")
+    bools[:autostart] = (autostart ? "yes" : "no")
+    cols.merge bools
+  end
+  
+  def colors
+    [ :bg, :leftbg, :lefticon, :rightbg, :rightbghover, :righticon, :righticonhover, :text, :slider, :track, :border, :loader ]
+  end
+  
+end
