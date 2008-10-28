07f6acd63b22a8760ddc45f5876550866bec9292
Andrew Neil <andrew.jr.neil@gmail.com>
2008-10-28 18:47:00 +0000

    The AudioPlayerConfig model has default setter methods for all colors. Regardless of whether the input is in #ABCDEF or 0xABCDEF format, the latter is what is saved to the db.

--- app/models/audio_player_config.rb
+++ app/models/audio_player_config.rb
@@ -24,8 +24,78 @@ class AudioPlayerConfig < ActiveRecord::Base
     cols.merge bools
   end
   
+  
+  # This badly needs refactored!
+  def bg=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:bg] = "0x#{$2}"
+    end
+  end
+  def leftbg=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:leftbg] = "0x#{$2}"
+    end
+  end
+  def lefticon=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:lefticon] = "0x#{$2}"
+    end
+  end
+  def rightbg=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:rightbg] = "0x#{$2}"
+    end
+  end
+  def rightbghover=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:rightbghover] = "0x#{$2}"
+    end
+  end
+  def righticon=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:righticon] = "0x#{$2}"
+    end
+  end
+  def righticonhover=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:righticonhover] = "0x#{$2}"
+    end
+  end
+  def text=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:text] = "0x#{$2}"
+    end
+  end
+  def slider=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:slider] = "0x#{$2}"
+    end
+  end
+  def slider=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:slider] = "0x#{$2}"
+    end
+  end
+  def track=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:track] = "0x#{$2}"
+    end
+  end
+  def border=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:border] = "0x#{$2}"
+    end
+  end
+  def loader=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:loader] = "0x#{$2}"
+    end
+  end
+  
   def colors
     [ :bg, :leftbg, :lefticon, :rightbg, :rightbghover, :righticon, :righticonhover, :text, :slider, :track, :border, :loader ]
   end
+  
+  
   
 end
