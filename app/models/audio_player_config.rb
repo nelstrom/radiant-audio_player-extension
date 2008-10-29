c2f26213bea6597ffd53f40bad845a53faba484b
Andrew Neil <andrew.jr.neil@gmail.com>
2008-10-29 08:40:03 +0000

    Using bg_hex virtual attribute for form fields, which always displays hex value, and saves an "0xabcdef" value.

--- app/models/audio_player_config.rb
+++ app/models/audio_player_config.rb
@@ -26,6 +26,40 @@ class AudioPlayerConfig < ActiveRecord::Base
   
   
   # This badly needs refactored!
+  def bg_hex
+    bg = value_or_default(:bg)
+    if bg =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      "##{$2}"
+    end
+  end
+  def bg_hex=(input)
+    if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
+      self[:bg] = "0x#{$2}"
+    end
+  end
+  
+  def value_or_default(column)
+    defaults = {
+      :bg => "0xf8f8f8",
+      :leftbg => "0xeeeeee",
+      :lefticon => "0x666666",
+      :rightbg => "0xcccccc",
+      :rightbghover => "0x999999",
+      :righticon => "0x666666",
+      :righticonhover => "0xffffff",
+      :text => "0x666666",
+      :slider => "0x666666",
+      :track => "0xFFFFFF",
+      :border => "0x666666",
+      :loader => "0x9FFFB8"
+    }
+    if value = self[column] and !value.blank?
+      value
+    else
+      defaults[column]
+    end
+  end
+  
   def bg=(input)
     if input =~ /^(0x|#)([0-9A-Fa-f]{3,6})$/
       self[:bg] = "0x#{$2}"
