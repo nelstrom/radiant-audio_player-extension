11e2b3398e4831c3d047ac30276215dd3af15678
Andrew Neil <andrew.jr.neil@gmail.com>
2009-06-16 00:25:28 +0100

    Styled the audio config form.

--- app/controllers/admin/audio_player_config_controller.rb
+++ app/controllers/admin/audio_player_config_controller.rb
@@ -1,6 +1,8 @@
 class Admin::AudioPlayerConfigController < Admin::AbstractModelController
   model_class AudioPlayerConfig
   
+  helper "admin/audio"
+  
   def index
     flash[:notice] = "Audio config settings saved."
     redirect_to :controller => :audio, :action => :index
--- app/helpers/admin/audio_helper.rb
+++ app/helpers/admin/audio_helper.rb
@@ -1,12 +1,19 @@
 module Admin::AudioHelper
   
   def admin_player_params(audio_track)
+
     if config = AudioPlayerConfig.find_by_name("Admin")
       player_params = config.parameters_for_player
     else
       player_params = ["autostart=no","loop=no"]
     end
-    player_params += ["playerID=#{audio_track.id}","soundFile=#{audio_track.track.url}"]
+
+    if audio_track.class == Audio
+      player_params += ["playerID=#{audio_track.id}","soundFile=#{audio_track.track.url}"]
+    else
+      player_params += ["playerID=example","soundFile=#{audio_track}"]
+    end
+
     player_params.join("&amp;")
   end
   
