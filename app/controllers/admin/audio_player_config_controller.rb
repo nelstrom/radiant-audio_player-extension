5022a38edde3c684f6627eef080e9687f1ddb602
Andrew Neil <andrew.jr.neil@gmail.com>
2009-06-15 21:09:16 +0100

    Audio config index redirects to audio index

--- app/controllers/admin/audio_player_config_controller.rb
+++ app/controllers/admin/audio_player_config_controller.rb
@@ -1,3 +1,8 @@
 class Admin::AudioPlayerConfigController < Admin::AbstractModelController
   model_class AudioPlayerConfig
+  
+  def index
+    flash[:notice] = "Audio config settings saved."
+    redirect_to :controller => :audio, :action => :index
+  end
 end
--- audio_player_extension.rb
+++ audio_player_extension.rb
@@ -28,40 +28,38 @@ class AudioPlayerExtension < Radiant::Extension
     Inflector.inflections do |inflect|
       inflect.uncountable "audio" # cause 'audios' just sounds wrong!
     end
-    # load_configuration
     admin.tabs.add "Audio", "/admin/audio", :after => "Layouts", :visibility => [:all]
     Page.send :include, AudioTags
     AudioPage
   end
 
   def deactivate
-    # admin.tabs.remove "Audio Player"
   end
 
 
 
 
-  def load_configuration
-    Radiant::Config.destroy_all(["config.key LIKE ?", "audio_player%"])
-    load_yaml('audio_player') do |configurations|
-      configurations.each do |key, value|
-        if value.is_a?(Hash)
-          value.each do |k,v|
-            Radiant::Config["audio_player.#{key}_#{k}"] = v
-          end
-        end
-      end
-    end
-  end
+  # def load_configuration
+  #   Radiant::Config.destroy_all(["config.key LIKE ?", "audio_player%"])
+  #   # load_yaml('audio_player') do |configurations|
+  #   #   configurations.each do |key, value|
+  #   #     if value.is_a?(Hash)
+  #   #       value.each do |k,v|
+  #   #         Radiant::Config["audio_player.#{key}_#{k}"] = v
+  #   #       end
+  #   #     end
+  #   #   end
+  #   # end
+  # end
 
-  private
-
-    def load_yaml(filename)
-      config_path = File.join(RAILS_ROOT, 'config', 'extensions', 'audio_player')
-      filename = File.join(config_path, "#{filename}.yml")
-      raise AudioPlayerExtensionError.new("AudioPlayerExtension error: #{filename} doesn't exist. Run the install task and try again.") unless File.exists?(filename)
-      data = YAML::load_file(filename)
-      yield(data)
-    end
+  # private
+  # 
+  #   def load_yaml(filename)
+  #     config_path = File.join(RAILS_ROOT, 'config', 'extensions', 'audio_player')
+  #     filename = File.join(config_path, "#{filename}.yml")
+  #     raise AudioPlayerExtensionError.new("AudioPlayerExtension error: #{filename} doesn't exist. Run the install task and try again.") unless File.exists?(filename)
+  #     data = YAML::load_file(filename)
+  #     yield(data)
+  #   end
 
 end
